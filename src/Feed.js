af921452d5bc12891e86442f998ed8ba648a33b7
VaibhavKhushalani <vaibhavkhushlani04@gmail.com>
2022-07-27 02:31:12 +0530

    fetch data from firebase

--- src/Feed.js
+++ src/Feed.js
@@ -1,99 +1,123 @@
-import React , { useEffect , useState} from 'react' ;
-import './Feed.css';
-import CreateIcon from '@material-ui/icons/Create';
-import PhotoIcon from '@material-ui/icons/Photo';
-import YouTubeIcon from '@material-ui/icons/YouTube';
-import EventNoteIcon from '@material-ui/icons/EventNote';
-import CalendarViewDayIcon from '@material-ui/icons/CalendarViewDay';
-import InputOption from './inputOption';
-import Post from './Post' ;
-// import { db } from './firebase';
+import React, { useEffect, useState } from "react";
+import "./Feed.css";
+import CreateIcon from "@material-ui/icons/Create";
+import PhotoIcon from "@material-ui/icons/Photo";
+import YouTubeIcon from "@material-ui/icons/YouTube";
+import EventNoteIcon from "@material-ui/icons/EventNote";
+import CalendarViewDayIcon from "@material-ui/icons/CalendarViewDay";
+import InputOption from "./inputOption";
+import Post from "./Post";
+import { getDocs, colRef } from "./firebase";
 // import firebase from 'firebase';
 
 function Feed() {
-  const [input , setInput] = useState(' ');
-    const [posts,setPosts] = useState([]);
+  const [input, setInput] = useState(" ");
+  const [posts, setPosts] = useState([]);
+  const [allPosts, setAllPosts] = useState([]);
+
   // useEffect(() => {
-  //       db.collection("posts").orderBy("timestamp","desc") 
+  //       db.collection("posts").orderBy("timestamp","desc")
   //       .onSnapshot(Snapshot => (
   //         setPosts(Snapshot.docs.map(doc => ({
 
   //             id: doc.id,
   //             data: doc.data(),
-            
+
   //         }))
   //         )
-  //       ))      
+  //       ))
   //       }, []);
-// connect with firebase 
+  // connect with firebase
 
-  const sendPost  = async (event) => {
-  event.preventDefault();
-  if(input){
-
-  
-const res = await fetch(
-  'https://linkedin-clone-yt-80e9a-default-rtdb.firebaseio.com/userDataRecords.json',
-  {
-    method: "POST",
-    headers: {
-      "Content-Type":"application/json",
-    },
-    body: JSON.stringify({
-      input
-    })
-  }
-  ); 
-  if(res){
-    alert("Data Stored")
-  } 
-} 
-else{
-  alert("Please fill the data")
-}
+  const sendPost = async (event) => {
+    event.preventDefault();
+    if (input) {
+      const res = await fetch(
+        "https://linkedin-clone-yt-80e9a-default-rtdb.firebaseio.com/userDataRecords.json",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            input,
+          }),
+        }
+      );
+      if (res) {
+        alert("Data Stored");
+      }
+    } else {
+      alert("Please fill the data");
+    }
 
     // db.collection('posts').add({
     //   name:'Dakshita Sharma',
     //   description: 'this is a test',
     //   message: input,
     //   photoUrl:'',
-    //   timestamp: firebase.firestore.fieldvalue.serverTimestamp() 
+    //   timestamp: firebase.firestore.fieldvalue.serverTimestamp()
     // })
-      setInput("");
+    setInput("");
   };
 
-  return (<div>
-    <div className='Feed'>
-  
-      <div className='Feed__input'>
-        <CreateIcon />
-          <form> 
-            <input value={input} onChange={(event) => setInput(event.target.value)} type='text' placeholder='Create a post'/>
-            <button onClick ={sendPost} type='submit'> Send</button>
-          </form>
-      </div>
-      <div className='Feed__inputOption'>
-        <InputOption Icon={PhotoIcon} title ='Photo'  color ='#005ce6' />
-        <InputOption Icon={YouTubeIcon} title ='Video'  color ='#009900' />
-        <InputOption Icon={EventNoteIcon} title ='Event'  color ='#e68a00' />
-        <InputOption Icon={CalendarViewDayIcon} title ='Write article'  color ='#cc2900' />
-      </div>
-  </div>
-  {posts.map(({id , data: { name , description , message, photoUrl} }) => {
-    <Post 
-      keys={id}
-      name={name}
-      description={description}
-      photoUrl={photoUrl}
-    />
-  })}
-    <Post 
-    name='Dakshita Sharma'
-    description='nsfjsdgdf'
-    message='mskgdgfdkg'
-    />
-</div>  
-  );
+  useEffect(() => {
+    const getPost = () => {
+      let filterData = [];
 
+      getDocs(colRef).then((snapshot) => {
+        snapshot.docs.forEach((docs) => {
+          filterData.push({ ...docs.data(), id: docs.id });
+          setAllPosts(filterData);
+        });
+      });
+    };
+    getPost();
+  }, []);
+  console.log(allPosts);
+  return (
+    <div>
+      <div className="Feed">
+        <div className="Feed__input">
+          <CreateIcon />
+          <form>
+            <input
+              value={input}
+              onChange={(event) => setInput(event.target.value)}
+              type="text"
+              placeholder="Create a post"
+            />
+            <button onClick={sendPost} type="submit">
+              {" "}
+              Send
+            </button>
+          </form>
+        </div>
+        <div className="Feed__inputOption">
+          <InputOption Icon={PhotoIcon} title="Photo" color="#005ce6" />
+          <InputOption Icon={YouTubeIcon} title="Video" color="#009900" />
+          <InputOption Icon={EventNoteIcon} title="Event" color="#e68a00" />
+          <InputOption
+            Icon={CalendarViewDayIcon}
+            title="Write article"
+            color="#cc2900"
+          />
+        </div>
+      </div>
+      {posts.map(({ id, data: { name, description, message, photoUrl } }) => {
+        <Post
+          keys={id}
+          name={name}
+          description={description}
+          photoUrl={photoUrl}
+        />;
+      })}
+      <Post
+        name="Dakshita Sharma"
+        description="nsfjsdgdf"
+        message="mskgdgfdkg"
+      />
+    </div>
+  );
 }
-export default Feed
+export default Feed;
--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,29 @@
-import firebase from 'firebase';
-
+import {initializeApp} from "firebase/app";
+import { getFirestore,collection,getDocs } from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: "AIzaSyDbxyriCItJNtkE8CCLzbajhxweuDuljng",
-  authDomain: "linkedin-clone-yt-80e9a.firebaseapp.com",
-  projectId: "linkedin-clone-yt-80e9a",
-  storageBucket: "linkedin-clone-yt-80e9a.appspot.com",
-  messagingSenderId: "996074154419",
-  appId: "1:996074154419:web:953be38dfb11c5d8dd43c6"
+  apiKey: "AIzaSyDiZqBAHLHclabneyMVLW0xbMvuKNAS4HE",
+  authDomain: "linkedin-c15d8.firebaseapp.com",
+  projectId: "linkedin-c15d8",
+  storageBucket: "linkedin-c15d8.appspot.com",
+  messagingSenderId: "1051177232842",
+  appId: "1:1051177232842:web:1ece5f2699bcdb9d390733",
+  measurementId: "G-4YVRG3PCEJ",
 };
 
-  const firebaseApp = firebase.initializeApp(firebaseConfig);
+initializeApp(firebaseConfig);
 
-  const db = firebaseApp.getFirestore();
-  const auth = firebase.auth();
+const db = getFirestore();
+const colRef =collection(db,'userDataRecords')
 
-  export { db , auth };
+// const auth = firebase.auth();
 
+export {getDocs,colRef };
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDbxyriCItJNtkE8CCLzbajhxweuDuljng",
+//   authDomain: "linkedin-clone-yt-80e9a.firebaseapp.com",
+//   projectId: "linkedin-clone-yt-80e9a",
+//   storageBucket: "linkedin-clone-yt-80e9a.appspot.com",
+//   messagingSenderId: "996074154419",
+//   appId: "1:996074154419:web:953be38dfb11c5d8dd43c6"
+// };
