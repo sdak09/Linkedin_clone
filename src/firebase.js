f78b2796f76214df8ba595075d89e4e3ddd9f131
VaibhavKhushalani <vaibhavkhushlani04@gmail.com>
2022-08-03 14:15:33 +0530

    firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import {initializeApp} from "firebase/app";
 import { getFirestore,collection,getDocs } from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: "AIzaSyDiZqBAHLHclabneyMVLW0xbMvuKNAS4HE",
-  authDomain: "linkedin-c15d8.firebaseapp.com",
-  projectId: "linkedin-c15d8",
-  storageBucket: "linkedin-c15d8.appspot.com",
-  messagingSenderId: "1051177232842",
-  appId: "1:1051177232842:web:1ece5f2699bcdb9d390733",
-  measurementId: "G-4YVRG3PCEJ",
+  apiKey: "AIzaSyDbxyriCItJNtkE8CCLzbajhxweuDuljng",
+  authDomain: "linkedin-clone-yt-80e9a.firebaseapp.com",
+  //databaseURL: "https://linkedin-clone-yt-80e9a-default-rtdb.firebaseio.com",
+  projectId: "linkedin-clone-yt-80e9a",
+  storageBucket: "linkedin-clone-yt-80e9a.appspot.com",
+  messagingSenderId: "996074154419",
+  appId: "1:996074154419:web:953be38dfb11c5d8dd43c6"
 };
 
 initializeApp(firebaseConfig);
